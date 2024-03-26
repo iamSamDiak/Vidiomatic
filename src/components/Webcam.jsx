8924d8d390c72a3a6b646bbd315f2eea878fdf5d
iamsamdiak <thediakarthur@gmail.com>
2024-03-26 14:18:59 +0100

    added errors handler and tested in multiple browsers except Safari

--- src/components/Webcam.jsx
+++ src/components/Webcam.jsx
@@ -8,26 +8,30 @@ function Webcam(){
     const videoElement = useRef(null)
     const webcam = useRef(null)
     const streamWebcam = useRef(null)
+    const card = useRef(null)
     const { messageCard, setMessageCard } = useContext(Context)
+
+    const errorsHandler = (err) => {
+        let _err = err.toString()
+        switch(_err){
+            case _err.includes("Must be handling a user gesture if there isn't already an element in Picture-in-Picture."):
+                return "Vérifiez que votre webcam n'est pas déjà en cours d'utilisation et réessayer."
+            case "NotReadableError: Device in use":
+                return "La webcam est déjà en cours d'utilisation. Fermez l'application qui l'utilise et recommencez."
+            case "InvalidStateError: Failed to execute 'requestPictureInPicture' on 'HTMLVideoElement': Metadata for the video element are not loaded yet.":
+                return "Une erreur est survenue. Veuillez réessayez ultérieurement."
+            case "TypeError: videoElement.current.requestPictureInPicture is not a function":
+                return "Votre navigateur ne supporte pas PIP. Nous vous recommandons un autre navigateur comme Chrome, Edge, Opera..."
+            default:
+                return _err
+        }
+    }
 
     useEffect(()=>{
         videoElement.current = document.querySelector(".webcam")
         webcam.current = document.querySelector(".webcam-button")
+        card.current = document.querySelector(".card")
     }, [])
-    
-    const activateWebcam = async () => {
-        accessWebcam().then(res => {
-            console.log(streamWebcam)
-            if (res){
-              webcam.current.textContent = res
-              setTimeout(() => {togglePictureInPicture()}, 300)
-            }
-            else {
-                webcam.current.textContent = "Activer la webcam"
-                console.log("PIP annulé")
-            }
-        })
-    }
 
     const accessWebcam = async () => {
         webcam.current.textContent = "Chargement..."
@@ -37,7 +41,6 @@ function Webcam(){
                     track.stop()
                 });
                 videoElement.current.srcObject = null
-                console.log("webcam")
                 return "Activer la webcam"
             } else {
                 const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
@@ -49,26 +52,78 @@ function Webcam(){
             }
         }
         catch (err){
+            console.log(err)
             setMessageCard({
-                message: err,
+                message: errorsHandler(err),
                 type: "error"
             })
+
+            card.current.classList.add("shown")
+            setTimeout(() => {
+                card.current.classList.remove("shown")
+            }, 5000)
+
         }
     }
 
     const togglePictureInPicture = () => {
-        console.log("here")
         if (document.pictureInPictureElement) {
           document.exitPictureInPicture(); // Exit PIP mode
-        } else {
-          videoElement.current.requestPictureInPicture() // Enter PIP mode
+          return
+        }
+
+        try {
+            videoElement.current.requestPictureInPicture() // Enter PIP mode
             .then(() => {
-              console.log("Video entered Picture-in-Picture mode.");
+                console.log("Video entered Picture-in-Picture mode.");
             })
             .catch(error => {
-              console.error("Error entering Picture-in-Picture mode:", error);
+                setMessageCard({
+                    message: errorsHandler(error),
+                    type: "error"
+                })
+                card.current.classList.add("shown")
+                setTimeout(() => {
+                    card.current.classList.remove("shown")
+                }, 5000)
+                console.error("Error entering Picture-in-Picture mode:", error.toString());
+                webcam.current.textContent = "Activer la webcam"
             });
+        } catch (err){
+            console.log(err)
+            setMessageCard({
+                message: errorsHandler(err),
+                type: "error"
+            })
+
+            card.current.classList.add("shown")
+            setTimeout(() => {
+                card.current.classList.remove("shown")
+            }, 5000)
+
+            if (videoElement.current.srcObject){
+                streamWebcam.current.getTracks().forEach((track) => {
+                    track.stop()
+                });
+                videoElement.current.srcObject = null
+                webcam.current.textContent = "Activer la webcam"
+            }
         }
+    }
+
+    const activateWebcam = async () => {
+        accessWebcam().then(res => {
+            console.log(streamWebcam)
+            if (res){
+              webcam.current.textContent = res
+              //togglePictureInPicture()
+              setTimeout(() => {togglePictureInPicture()}, 300)
+            }
+            else {
+                webcam.current.textContent = "Activer la webcam"
+                console.log("PIP annulé")
+            }
+        })
     }
 
     return(
